edf89f9af523d11b613c0dce1fbabe42a1078e94
Lakesh Kumar <lakeshkumarkhatri7@gmail.com>
2026-05-24 12:37:02 +0500

    Prepare config for deployment

--- Config/config.py
+++ Config/config.py
@@ -1,11 +1,21 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 
-API_KEY = os.getenv("GOOGLE_API_KEY")
+API_KEY = (
+    st.secrets.get(
+        "GOOGLE_API_KEY",
+        os.getenv(
+            "GOOGLE_API_KEY"
+        )
+    )
+)
 
-client = genai.Client(api_key=API_KEY)
+client = genai.Client(
+    api_key=API_KEY
+)
 
 MODEL_NAME = "gemini-3.1-flash-lite"
